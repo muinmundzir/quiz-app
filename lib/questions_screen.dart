6809ae1f348d294a80e25881e1382442853a7b26
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-03-16 20:57:06 +0700

    feat: implement question and answer

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,18 +14,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Questions Screen',
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton('hehe', () { })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(answer, () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
